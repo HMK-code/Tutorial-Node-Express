eaa408a462e641a8d0a5da6923da3d67e703dffc
Hamdi <HMKepenek@gmail.com>
2021-10-27 00:18:53 +0300

    Express Router

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const dishRouter = require('./routers/dishRouter')
 
 const hostname = "localhost";
-const port = 3000;
+const port = 3003;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json())
 
-app.use(express.static(__dirname+ '/public'));
+app.use('/dishes', dishRouter)
 
-app.use((req, res, next) => {
+ app.use(express.static(__dirname+ '/public')); 
+
+ app.use((req, res, next) => {
     res.statusCode = 200
     res.setHeader("Content-Type", "text/html");
     res.end("<html><body><h1>This is an Express Server</h1></body></html>")
 });
-
+ 
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
